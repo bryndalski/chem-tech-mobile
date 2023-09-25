6c529754c6e83331c53b5c6cc36b6f79356b6e48
bryndalski <kubabryndal@gmail.com>
2023-09-25 20:36:21 +0200

    created auto language setup for i18n

--- app/src/localization/i18n.ts
+++ app/src/localization/i18n.ts
@@ -3,6 +3,14 @@ import {initReactI18next} from 'react-i18next';
 import en from './en/en.json';
 import pl from './pl/pl.json';
 
+import {Platform, NativeModules} from 'react-native';
+
+const deviceLanguage =
+  Platform.OS === 'ios'
+    ? NativeModules.SettingsManager.settings.AppleLocale ||
+      NativeModules.SettingsManager.settings.AppleLanguages[0] //iOS 13
+    : NativeModules.I18nManager.localeIdentifier;
+
 export const languageResources = {
   pl: {translation: pl},
   en: {translation: en},
@@ -10,8 +18,8 @@ export const languageResources = {
 
 i18next.use(initReactI18next).init({
   compatibilityJSON: 'v3',
-  lng: 'pl',
-  fallbackLng: 'pl',
+  lng: deviceLanguage === 'en_PL' ? 'pl' : 'en',
+  fallbackLng: 'en',
   resources: languageResources,
 });
 
