4b7c797f513a53be30b6758b867c3aa045823666
bryndalski <kubabryndal@gmail.com>
2023-10-05 10:29:17 +0200

    created primary login button (#4)

--- app/App.tsx
+++ app/App.tsx
@@ -12,18 +12,18 @@ import {ViewNames, Welcome, Login} from '@views/index';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import {StyleSheet} from 'react-native';
 
 function App(): JSX.Element {
   const Stack = createNativeStackNavigator();
 
   return (
-    <SafeAreaView style={{flex: 1}}>
+    <SafeAreaView style={styles.container}>
       <NavigationContainer>
         <Stack.Navigator
           screenOptions={{
             headerShown: false,
-          }}
-        >
+          }}>
           <Stack.Screen name={ViewNames.Welcome} component={Welcome} />
           <Stack.Screen name={ViewNames.Login} component={Login} />
         </Stack.Navigator>
@@ -31,5 +31,11 @@ function App(): JSX.Element {
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default App;
---
+++ app/src/components/buttons/button_primary/button_primary.tsx
@@ -0,0 +1,60 @@
+import {Text, TouchableOpacity, StyleSheet} from 'react-native';
+import React from 'react';
+import {Colors} from 'src/assets/colors/colors.enum';
+interface ButtonPrimaryProps {
+  text: string;
+  callback: () => void;
+  disabled?: boolean;
+}
+
+export function ButtonPrimary(props: ButtonPrimaryProps) {
+  return (
+    <TouchableOpacity
+      style={[
+        styles.container,
+        props.disabled ? styles.color_disabled : styles.color_enabled,
+      ]}
+      onPress={() => props.disabled && props.callback()}>
+      <Text
+        style={[
+          styles.text,
+          props.disabled ? styles.text_disabled : styles.text_enabled,
+        ]}>
+        {props.text}
+      </Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    borderRadius: 50,
+    minHeight: 50,
+    display: 'flex',
+    alignContent: 'center',
+    justifyContent: 'center',
+  },
+  color_enabled: {
+    backgroundColor: Colors.primary_blue,
+  },
+  color_disabled: {
+    backgroundColor: Colors.white,
+    borderColor: Colors.primary_blue,
+    borderWidth: 2,
+  },
+  text: {
+    color: '#FFF',
+    textAlign: 'center',
+    fontFamily: 'Roboto',
+    fontSize: 20,
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    letterSpacing: 0.6,
+  },
+  text_disabled: {
+    color: Colors.primary_blue,
+  },
+  text_enabled: {
+    color: Colors.white,
+  },
+});
